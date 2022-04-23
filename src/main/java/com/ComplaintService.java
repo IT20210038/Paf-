13a9f6a0931570356629aed50f37b84e8b4ff6f5
IshaniSenanayake <ishani.0127@gmail.com>
2022-04-24 00:21:05 +0530

    My commit for complaints

--- src/main/java/com/ComplaintService.java
+++ src/main/java/com/ComplaintService.java
@@ -1,5 +1,27 @@
 package com;
 
-public class ComplaintService {
+import model.Complaint; 
 
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*; 
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+
+
+@Path("/Items")
+public class ComplaintService {
+	Complaint complainObj = new Complaint(); 
+	@GET
+	@Path("/") 
+	@Produces(MediaType.TEXT_HTML) 
+	public String readComplaints() 
+	 { 
+	 return "Hello"; 
+	 } 
 }
--- src/main/java/model/Complaint.java
+++ src/main/java/model/Complaint.java
@@ -145,4 +145,30 @@ public class Complaint {
 		 return output; 
 		}
 		
+		public String deleteComplaint(String accountno) 
+		 { 
+		 String output = ""; 
+		 try
+		 { 
+		 Connection con = connect(); 
+		 if (con == null) 
+		 {return "Error while connecting to the database for deleting."; } 
+		 // create a prepared statement
+		 String query = "delete from items where accountno=?"; 
+		 PreparedStatement preparedStmt = con.prepareStatement(query); 
+		 // binding values
+		 preparedStmt.setInt(1, Integer.parseInt(accountno)); 
+		 // execute the statement
+		 preparedStmt.execute(); 
+		 con.close(); 
+		 output = "Deleted successfully"; 
+		 } 
+		 catch (Exception e) 
+		 { 
+		 output = "Error while deleting the item."; 
+		 System.err.println(e.getMessage()); 
+		 } 
+		 return output; 
+		 } 
+		
 }
